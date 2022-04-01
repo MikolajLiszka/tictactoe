9555459837f4ee0f5fbc36703becfcf0ac2fb9ca
MikolajLiszka <mikolaj.liszka@gmail.com>
2022-04-01 11:01:42 +0200

    kolko i krzyzyk reset

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -1,41 +1,54 @@
-
-let boxes = document.querySelectorAll('.box');
+let boxes = document.querySelectorAll(".box");
+const btnReset = document.getElementById("reset");
+btnReset.addEventListener("click", () => {
+    console.log("click");
+    boxes.forEach((element) => {
+        element.innerText = "";
+    })
+})
 boxes = Array.from(boxes);
-console.log(boxes);
+// console.log(btnReset);
 
 playerX = "X";
 playerO = "O";
 let currentPlayer = playerX;
 
-let winningConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+let winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-function won(){
-    winningConditions.forEach(function(condition){
-    let check = condition.every(id => boxes[id].innerText == currentPlayer);
-    if (check == true)
-    {
-        alert("You won")
+function won() {
+  winningConditions.forEach(function (condition) {
+    let check = condition.every((id) => {
+        // console.log(id);
+        return boxes[id].innerText == currentPlayer
+    });
+    if (check) {
+      alert("You won");
     }
-})
+  });
 }
 
-
-boxes.forEach(function(box){
-    box.addEventListener('click', function(){
-        if(box.innerText != "") return;
-        if (currentPlayer == playerX)
-        {
-            box.innerText = playerX;
-            won();
-            currentPlayer = playerO;
-        }
-        else
-        {
-            box.innerText = playerO;
-            won();
-            currentPlayer = playerX;
-        }
-        
-    })
-})
-
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    if (box.innerText != "") {
+        return;
+    }
+    if (currentPlayer == playerX) {
+      box.innerText = playerX;
+      won();
+      currentPlayer = playerO;
+    } else {
+      box.innerText = playerO;
+      won();
+      currentPlayer = playerX;
+    }
+  });
+});
